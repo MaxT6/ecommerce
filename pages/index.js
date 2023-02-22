a2fb83601fdcc6d72764a09de8678cc793cfe301
MaxT6 <maximiliant6@gmail.com>
2023-02-22 13:09:03 -0500

    updated Product component to display products

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,16 @@ import { Product, FooterBanner, HeroBanner  } from '@/components';
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0] }/>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0] }/> {/* This first checks that we have banner data*/}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product  key={product._id} product={product}/>)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footBanner={bannerData && bannerData[0]} />
     </>
   )
 }
