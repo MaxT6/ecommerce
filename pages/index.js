d1fc63c829592d2c2ab157a45d6df2dbb1526d8a
MaxT6 <maximiliant6@gmail.com>
2023-02-22 12:18:42 -0500

    Created sanity client and now am displaying banner items from Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
+
+import { client } from '@/lib/client';
 import { Product, FooterBanner, HeroBanner  } from '@/components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0] }/>
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers</p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+// instead of useEffect() Next.js uses getServerSideProps to preRender a page with data from the client
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
 }
 
 export default Home
