af10124aa2a763fe85c9e1b08b482d5767e4bd8b
MaxT6 <maximiliant6@gmail.com>
2023-02-22 15:44:30 -0500

    set up Layout, Navbar, and Footer Components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head'; // Head element that appears above the body that provides meta data for the website
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>MT Tech Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { Layout } from '@/components'; //we will be passing the current component page the user is accessing to the Layout component by using the children prop in Layout
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+     <Component {...pageProps} />
+    </Layout>
+  )
 }
